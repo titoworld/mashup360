87133c50f1ef7f76348103140fcc7d36e3daa27f
Albert Farré Puche <albertfarre@gmail.com>
2013-11-06 13:08:12 +0100

    Nou sistema de control d'errors 403

--- uploads/360POIView/error404.php
+++ uploads/360POIView/error404.php
@@ -1,18 +1,15 @@
-<html>
-	<head>
-		<? include("../../pages/header.php") ?>
-		<title>Error 404.</title>
-	</head>
-	<body>
-		<? 
-		    $POIname = isset($_GET['idpoi']) ? $_GET['idpoi'] : null ;
-			//$dir_path = str_replace( $_SERVER['DOCUMENT_ROOT'], "", dirname(realpath(__FILE__)) ) . DIRECTORY_SEPARATOR; 
-			$carpeta_final = $POIname."/";  
-			$HTMLs = glob($carpeta_final."*.html");	
-			rename($HTMLs[0], $carpeta_final ."index.html");
-		?>
-	<script>window.location.href = "index.html";</script>
-	<div style="width:400px;margin:auto;"><img src="<? echo IMAGE_DOMAIN ?>404.jpg" alt="error" />
-	<div id="textLink" style="text-align:center;"><span id="textIndex" ><a style="color:black;text-decoration: none" href="index.php">Tornar al Index</a></span></div></div>
-	</body>
-</html>
+
+<? 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$gets= $_SERVER['REDIRECT_QUERY_STRING'];
+$getvars= explode("=",$gets);
+$POIname=$getvars[1];
+//$POIname = isset($_GET['idpoi']) ? $_GET['idpoi'] : null ;
+//$dir_path = str_replace( $_SERVER['DOCUMENT_ROOT'], "", dirname(realpath(__FILE__)) ) . DIRECTORY_SEPARATOR; 
+$carpeta_final = $POIname."/";  
+$HTMLs = glob($carpeta_final."*.html"); 
+//rename($HTMLs[0],realpath(dirname(__FILE__)).'index.html');                   
+rename($HTMLs[0], $carpeta_final ."index.html");
+?>
+<script>window.location.reload();</script>
